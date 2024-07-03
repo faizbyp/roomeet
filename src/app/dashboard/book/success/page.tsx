269481ffb9728a1ce0623a6a556555621b2e475b
faizbyp <faizbyp@gmail.com>
2024-07-03 10:10:51 +0700

    add: checkmark on book success

--- src/app/dashboard/book/success/page.tsx
+++ src/app/dashboard/book/success/page.tsx
@@ -1,3 +1,4 @@
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 import { Box, Button } from "@mui/material";
 import Link from "next/link";
 
@@ -6,13 +7,14 @@ const SuccessPage = () => {
     <>
       <Box
         sx={{
-          height: "100svh",
+          height: "80svh",
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
+        <CheckCircleIcon className="size-36" />
         <Box sx={{ marginBottom: 4 }}>Success book meeting</Box>
         <Link href="/dashboard" passHref>
           <Button variant="contained">Back to dashboard</Button>
