c1dcdf542ff4113de742170844b3185e39d8845d
faizbyp <faizbyp@gmail.com>
2024-07-12 16:04:47 +0700

    add: approval status filter on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-import { Box, Button, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Typography,
+} from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import moment from "moment";
 import { useSession } from "next-auth/react";
@@ -11,7 +20,8 @@ import useSWR from "swr";
 const AdminPage = () => {
   const { data } = useSession();
   const [date, setDate] = useState<any>("");
-  const url = `/book?book_date=${date}`;
+  const [status, setStatus] = useState<any>("");
+  const url = `/book?book_date=${date}&approval=${status}`;
   const {
     data: books,
     error,
@@ -28,15 +38,35 @@ const AdminPage = () => {
     console.log(d);
   };
 
+  const handleStatus = (e: any) => {
+    const s = e.target.value;
+    setStatus(s);
+    console.log(s);
+  };
+
   return (
     <>
-      <h1>Admin Page</h1>
-      <DatePicker
-        label="Search Date"
-        onChange={handleDate}
-        // slotProps={{ field: { clearable: true } }}
-        sx={{ mb: 16 }}
-      />
+      <Box sx={{ my: 16 }}>
+        <Typography variant="h1">Admin Page</Typography>
+      </Box>
+      <Box sx={{ display: "flex", gap: 8 }}>
+        <DatePicker
+          label="Search Date"
+          onChange={handleDate}
+          // slotProps={{ field: { clearable: true } }}
+          sx={{ mb: 16 }}
+        />
+        <FormControl fullWidth>
+          <InputLabel>Approval Status</InputLabel>
+          <Select defaultValue="" value={status} label="Approval" onChange={handleStatus}>
+            <MenuItem value="">---</MenuItem>
+            <MenuItem value="pending">Pending</MenuItem>
+            <MenuItem value="approved">Approved</MenuItem>
+            <MenuItem value="rejected">Rejected</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+
       <Box sx={{ display: "flex", gap: 8, flexDirection: "column", px: 16 }}>
         {isLoading ? (
           <p>Loading...</p>
