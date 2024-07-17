997a7824a14c4cf521cd9f26e9300a43f4caf415
faizbyp <faizbyp@gmail.com>
2024-07-17 11:04:18 +0700

    add: room search query to calendar

--- src/components/room/components/Room.tsx
+++ src/components/room/components/Room.tsx
@@ -1,16 +1,16 @@
 import CardEvent from "@/common/CardEvent";
 import TitleRoom from "./TitleRoom";
-import { MenuItem, Select } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import BigCalendar from "./BigCalendar";
 import { axiosAuth } from "@/lib/axios";
 import useSWR from "swr";
 
 const Room = () => {
-  const [room, setRoom] = useState();
+  const [room, setRoom] = useState("");
   const [events, setEvents] = useState();
 
-  const url = `/book`;
+  const url = `/book?room=${room}`;
   const {
     data: books,
     error,
@@ -40,14 +40,28 @@ const Room = () => {
     }
   }, [books]);
 
-  console.log(books);
-  console.log(events);
+  const handleRoom = (e: any) => {
+    const r = e.target.value;
+    setRoom(r);
+    console.log(r);
+  };
 
   return (
     <>
       <div className="px-4 mt-4">
         <div className="flex justify-end">
-          <TitleRoom name={"ROOM 12"} />
+          {/* <TitleRoom name={"ROOM 12"} /> */}
+          <FormControl fullWidth>
+            <InputLabel>Room</InputLabel>
+            <Select defaultValue="" value={room} label="Approval" onChange={handleRoom}>
+              <MenuItem value="">---</MenuItem>
+              <MenuItem value="ROOM001">ROOM001</MenuItem>
+              <MenuItem value="ROOM002">ROOM002</MenuItem>
+              <MenuItem value="ROOM003">ROOM003</MenuItem>
+              <MenuItem value="ROOM004">ROOM004</MenuItem>
+              <MenuItem value="ROOM005">ROOM005</MenuItem>
+            </Select>
+          </FormControl>
         </div>
         {isLoading && events ? <p>Loading...</p> : <BigCalendar events={events} />}
         {error && <p>Error fetching data</p>}
