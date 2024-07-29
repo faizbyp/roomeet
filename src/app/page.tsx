72b02d9df748fad3550a5a4109170f70a7a9a0b0
faizbyp <faizbyp@gmail.com>
2024-07-29 08:59:01 +0700

    change: index page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,39 @@
 "use client";
 
 import { Box, Typography, Button } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function HomePage() {
-  return <>
-    <Box sx={{minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around"}}>
-      <Typography variant="h4">
-        KPN Corp
-      </Typography>
-      <Typography variant="subtitle1" sx={{color: "primary.main", textTransform: "uppercase", fontWeight: "bold"}}>Book your meeting room</Typography>
-      <Link href="/dashboard" passHref>
-        <Button variant="contained" className="btn-primary">Start Now</Button>
-      </Link>
-    </Box>
-  </>;
+  return (
+    <>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        {/* <Image
+          src="/img/kpn-corp.png"
+          alt="KPN Corp"
+          width={431}
+          height={117}
+          sizes="50%, 50%"
+          style={{ borderRadius: 16 }}
+        /> */}
+        <Typography variant="h1" sx={{ color: "primary.main" }}>
+          Roomeet
+        </Typography>
+        <Typography variant="h3" sx={{ textTransform: "uppercase", fontWeight: "bold" }}>
+          Book your meeting room
+        </Typography>
+        <Link href="/dashboard" passHref>
+          <Button variant="contained">Start Now</Button>
+        </Link>
+      </Box>
+    </>
+  );
 }
