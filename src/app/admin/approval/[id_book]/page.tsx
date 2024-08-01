963a97920fc77368351ad6508791ed5500b00938
faizbyp <faizbyp@gmail.com>
2024-08-01 14:56:48 +0700

    fix: approval page style

--- src/app/admin/approval/[id_book]/page.tsx
+++ src/app/admin/approval/[id_book]/page.tsx
@@ -2,9 +2,10 @@
 
 import ApprovalAction from "@/components/admin/ApprovalAction";
 import { axiosAuth } from "@/lib/axios";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Skeleton, Typography } from "@mui/material";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import moment from "moment";
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 const Approval = ({ params }: { params: { id_book: string } }) => {
@@ -32,40 +33,57 @@ const Approval = ({ params }: { params: { id_book: string } }) => {
     <>
       {book ? (
         <>
-          <p>{`Approval - ${book.approval}`}</p>
-          <Typography variant="h1">{book.agenda}</Typography>
+          <Typography
+            sx={[
+              {
+                mb: 16,
+              },
+              book.approval === "pending" && {
+                color: "warning.main",
+              },
+              book.approval === "rejected" && {
+                color: "error.main",
+              },
+              book.approval === "approved" && {
+                color: "success.main",
+              },
+            ]}
+          >{`Approval - ${book.approval}`}</Typography>
+          <Typography>{book.id_ticket}</Typography>
+          <Typography variant="h1" sx={{ color: "primary.light" }}>
+            {book.agenda}
+          </Typography>
           <Typography variant="h2">{book.id_ruangan}</Typography>
-          <Typography>{`${book.prtcpt_ctr} participants`}</Typography>
-          <p>
+          <Box sx={{ display: "flex", alignItems: "center", gap: 4 }}>
+            <PeopleAltIcon />
+            <Typography>{book.prtcpt_ctr} participants</Typography>
+          </Box>
+          <Box sx={{ my: 16 }}>
+            <Typography>{moment(book.book_date).format("dddd, DD/MM/YYYY")}</Typography>
+            <Typography>{`${book.time_start.slice(0, 5)} - ${book.time_end.slice(
+              0,
+              5
+            )}`}</Typography>
+          </Box>
+          <Typography sx={{ mb: 16 }}>
             Created by:
             <br />
             {book.username}
-          </p>
-          <TextField
-            fullWidth
-            label="Remark"
-            variant="filled"
-            value={book.remark ? book.remark : "---"}
-            disabled
-            sx={{ mb: 8 }}
-          />
-          <TextField
-            fullWidth
-            label="Booking Date"
-            variant="filled"
-            value={moment(book.book_date).format("dddd, DD/MM/YYYY")}
-            disabled
-          />
-          <p>Time</p>
-          <TextField label="Start Time" variant="filled" value={book.time_start} disabled />
-          <TextField label="End Time" variant="filled" value={book.time_end} disabled />
-
+          </Typography>
+          <Typography>Remark:</Typography>
+          <Typography sx={{ mb: 16 }}>{book.remark ? book.remark : "---"}</Typography>
+          {book.reject_note && (
+            <Box>
+              <Typography sx={{ color: "error.light" }}>Reject Note:</Typography>
+              <Typography>{book.reject_note}</Typography>
+            </Box>
+          )}
           {book.approval === "approved" || book.approval === "rejected" ? null : (
             <ApprovalAction id_book={params.id_book} props={book} />
           )}
         </>
       ) : (
-        <p>Loading...</p>
+        <Skeleton variant="rounded" width="100%" height={96} sx={{ bgcolor: "grey.700" }} />
       )}
     </>
   );
