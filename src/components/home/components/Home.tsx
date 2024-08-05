b5221a041db105a0b1e1c4175bca667953e48a06
faizbyp <faizbyp@gmail.com>
2024-08-05 11:23:00 +0700

    add: home confirm dialog and loading

--- src/components/home/components/Home.tsx
+++ src/components/home/components/Home.tsx
@@ -10,6 +10,7 @@ import {
   Typography,
   Grid,
   Box,
+  Skeleton,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { CardRooms } from "./HomeCardRoom";
@@ -21,6 +22,7 @@ import { axiosAuth } from "@/lib/axios";
 import toast from "react-hot-toast";
 import useSWR from "swr";
 import moment from "moment";
+import ConfirmationDialog from "@/common/ConfirmationDialog";
 
 const settings = {
   speed: 500,
@@ -61,7 +63,7 @@ const Home = () => {
         Notification.requestPermission();
       }
     }
-  }, []);
+  }, [data?.user.id_user]);
 
   const ciUrl = `/book/checkin/${data?.user.id_user}`;
   const coUrl = `/book/checkout/${data?.user.id_user}`;
@@ -126,114 +128,160 @@ const Home = () => {
       </Box>
       <DigitalClock />
 
-      {checkin && checkin?.data.length !== 0 && (
-        <>
-          <Typography sx={{ fontWeight: "bold", mb: 16, color: "primary.light" }}>
-            Check In
-          </Typography>
-
-          {checkin?.data.map((ci: any) => (
-            <>
-              <Box
-                sx={{ px: 24, py: 16, backgroundColor: "background.card", my: 16, borderRadius: 4 }}
-              >
-                <Typography variant="h2">{ci.agenda}</Typography>
-                <Typography variant="h3">{ci.id_ruangan}</Typography>
-                <Typography variant="h3" sx={{ fontWeight: "regular" }}>
-                  {`${ci.time_start} - ${ci.time_end} | ${moment(ci.book_date).format(
-                    "DD/MM/YYYY"
-                  )}`}
-                </Typography>
-                <Button
-                  variant="contained"
-                  fullWidth
-                  onClick={() => handleCheckIn(data?.user.id_user, ci.id_book)}
+      <Box sx={{ mb: 16 }}>
+        <Typography sx={{ fontWeight: "bold", mb: 16, color: "primary.light" }}>
+          Check In
+        </Typography>
+        {checkin ? (
+          checkin?.data.length !== 0 ? (
+            // Loading finished and data exist
+            checkin?.data.map((ci: any) => (
+              <>
+                <Box
+                  sx={{
+                    px: 24,
+                    py: 16,
+                    backgroundColor: "background.card",
+                    mt: 16,
+                    borderRadius: 4,
+                  }}
                 >
-                  Check In
-                </Button>
-              </Box>
-            </>
-          ))}
-        </>
-      )}
-
-      {checkout && checkout?.data.length !== 0 && (
-        <>
-          <Typography sx={{ fontWeight: "bold", my: 16, color: "primary.light" }}>
-            Check Out
-          </Typography>
-          {checkout?.data.map((co: any) => (
-            <>
-              <Box
-                sx={{ px: 24, py: 16, backgroundColor: "background.card", mb: 16, borderRadius: 4 }}
-              >
-                <Typography variant="h2">{co.agenda}</Typography>
-                <Typography variant="h3">{co.id_ruangan}</Typography>
-                <Typography variant="h3" sx={{ fontWeight: "regular" }}>
-                  {`${co.time_start} - ${co.time_end} | ${moment(co.book_date).format(
-                    "DD/MM/YYYY"
-                  )}`}
-                </Typography>
-                <Button
-                  variant="contained"
-                  fullWidth
-                  onClick={() => handleCheckOut(data?.user.id_user, co.id_book)}
-                >
-                  Check Out
-                </Button>
-              </Box>
-            </>
-          ))}
-        </>
-      )}
-
-      {books && books?.data.length !== 0 && (
-        <>
-          <Typography sx={{ fontWeight: "bold", my: 16, color: "primary.light" }}>
-            Nearest Meeting
-          </Typography>
-          {books?.data.map((book: any) => (
-            <Box
-              sx={{ px: 24, py: 16, backgroundColor: "background.card", mb: 16, borderRadius: 4 }}
-              key={book.id_book}
-            >
-              <Grid container>
-                <Grid item xs={8}>
-                  <Typography variant="h2">{book.agenda}</Typography>
-                  <Typography variant="h3">{book.id_room}</Typography>
-                  <Typography variant="h4" sx={{ fontWeight: "regular" }}>
-                    {`${book.time_start} - ${book.time_end} | ${moment(book.book_date).format(
+                  <Typography variant="h2">{ci.agenda}</Typography>
+                  <Typography variant="h3">{ci.id_ruangan}</Typography>
+                  <Typography variant="h3" sx={{ fontWeight: "regular" }}>
+                    {`${ci.time_start} - ${ci.time_end} | ${moment(ci.book_date).format(
                       "DD/MM/YYYY"
                     )}`}
                   </Typography>
-                </Grid>
-                <Grid item xs={4}>
-                  <Typography
-                    align="center"
-                    sx={[
-                      {
-                        color: "black",
-                        borderRadius: 2,
-                      },
-                      book.approval === "pending" && {
-                        backgroundColor: "warning.main",
-                      },
-                      book.approval === "rejected" && {
-                        backgroundColor: "error.main",
-                      },
-                      book.approval === "approved" && {
-                        backgroundColor: "success.main",
-                      },
-                    ]}
+                  <ConfirmationDialog
+                    title="Confirm Check In"
+                    desc="Are you sure you want to check in?"
+                    action="Check In"
+                    response={() => handleCheckIn(data?.user.id_user, ci.id_book)}
+                    type="button"
                   >
-                    {book.approval}
+                    {(showDialog: any) => (
+                      <Button variant="contained" fullWidth onClick={showDialog}>
+                        Check In
+                      </Button>
+                    )}
+                  </ConfirmationDialog>
+                </Box>
+              </>
+            ))
+          ) : (
+            // Loading finished and data dont exist
+            <Typography sx={{ color: "grey.500" }}>No upcoming meeting</Typography>
+          )
+        ) : (
+          // Loading UI
+          <Skeleton variant="rounded" width="100%" height={64} sx={{ bgcolor: "grey.700" }} />
+        )}
+      </Box>
+
+      <Box sx={{ mb: 16 }}>
+        <Typography sx={{ fontWeight: "bold", my: 16, color: "primary.light" }}>
+          Check Out
+        </Typography>
+        {checkout ? (
+          checkout?.data.length !== 0 ? (
+            checkout?.data.map((co: any) => (
+              <>
+                <Box
+                  sx={{
+                    px: 24,
+                    py: 16,
+                    backgroundColor: "background.card",
+                    mt: 16,
+                    borderRadius: 4,
+                  }}
+                >
+                  <Typography variant="h2">{co.agenda}</Typography>
+                  <Typography variant="h3">{co.id_ruangan}</Typography>
+                  <Typography variant="h3" sx={{ fontWeight: "regular" }}>
+                    {`${co.time_start} - ${co.time_end} | ${moment(co.book_date).format(
+                      "DD/MM/YYYY"
+                    )}`}
                   </Typography>
+                  <ConfirmationDialog
+                    title="Confirm Check Out"
+                    desc="Are you sure you want to check out?"
+                    action="Check Out"
+                    response={() => handleCheckOut(data?.user.id_user, co.id_book)}
+                    type="button"
+                  >
+                    {(showDialog: any) => (
+                      <Button variant="contained" fullWidth onClick={showDialog}>
+                        Check Out
+                      </Button>
+                    )}
+                  </ConfirmationDialog>
+                </Box>
+              </>
+            ))
+          ) : (
+            <Typography sx={{ color: "grey.500" }}>Please check in first</Typography>
+          )
+        ) : (
+          // Loading UI
+          <Skeleton variant="rounded" width="100%" height={64} sx={{ bgcolor: "grey.700" }} />
+        )}
+      </Box>
+
+      <Box sx={{ mb: 16 }}>
+        <Typography sx={{ fontWeight: "bold", my: 16, color: "primary.light" }}>
+          Nearest Meeting
+        </Typography>
+        {books ? (
+          books?.data.length !== 0 ? (
+            books?.data.map((book: any) => (
+              <Box
+                sx={{ px: 24, py: 16, backgroundColor: "background.card", mb: 16, borderRadius: 4 }}
+                key={book.id_book}
+              >
+                <Grid container>
+                  <Grid item xs={8}>
+                    <Typography variant="h2">{book.agenda}</Typography>
+                    <Typography variant="h3">{book.id_room}</Typography>
+                    <Typography variant="h4" sx={{ fontWeight: "regular" }}>
+                      {`${book.time_start} - ${book.time_end} | ${moment(book.book_date).format(
+                        "DD/MM/YYYY"
+                      )}`}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography
+                      align="center"
+                      sx={[
+                        {
+                          color: "black",
+                          borderRadius: 2,
+                        },
+                        book.approval === "pending" && {
+                          backgroundColor: "warning.main",
+                        },
+                        book.approval === "rejected" && {
+                          backgroundColor: "error.main",
+                        },
+                        book.approval === "approved" && {
+                          backgroundColor: "success.main",
+                        },
+                      ]}
+                    >
+                      {book.approval}
+                    </Typography>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Box>
-          ))}
-        </>
-      )}
+              </Box>
+            ))
+          ) : (
+            <Typography sx={{ color: "grey.500" }}>No meeting</Typography>
+          )
+        ) : (
+          // Loading UI
+          <Skeleton variant="rounded" width="100%" height={64} sx={{ bgcolor: "grey.700" }} />
+        )}
+      </Box>
     </Box>
   );
 };
