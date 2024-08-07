156ff6d81434ae9b379e2b979e3e2874e8d9189a
faizbyp <faizbyp@gmail.com>
2024-08-07 17:26:25 +0700

    fix: room page

--- src/components/room/components/Room.tsx
+++ src/components/room/components/Room.tsx
@@ -1,13 +1,13 @@
 import CardEvent from "@/common/CardEvent";
 import TitleRoom from "./TitleRoom";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import BigCalendar from "./BigCalendar";
 import { axiosAuth } from "@/lib/axios";
 import useSWR from "swr";
 
 const Room = () => {
-  const [room, setRoom] = useState("");
+  const [room, setRoom] = useState("ROOM001");
   const [events, setEvents] = useState();
 
   const url = `/book?room=${room}`;
@@ -48,28 +48,21 @@ const Room = () => {
 
   return (
     <>
-      <div className="px-4 mt-4">
-        <div className="flex justify-end">
-          {/* <TitleRoom name={"ROOM 12"} /> */}
-          <FormControl fullWidth>
-            <InputLabel>Room</InputLabel>
-            <Select defaultValue="" value={room} label="Approval" onChange={handleRoom}>
-              <MenuItem value="">---</MenuItem>
-              <MenuItem value="ROOM001">ROOM001</MenuItem>
-              <MenuItem value="ROOM002">ROOM002</MenuItem>
-              <MenuItem value="ROOM003">ROOM003</MenuItem>
-              <MenuItem value="ROOM004">ROOM004</MenuItem>
-              <MenuItem value="ROOM005">ROOM005</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-        {isLoading && events ? <p>Loading...</p> : <BigCalendar events={events} />}
-        {error && <p>Error fetching data</p>}
-        {/* <CalendarEvent /> */}
-        <CardEvent />
-        <CardEvent />
-        <p className="text-center text-neutral-100 pt-2">View More</p>
-      </div>
+      <Box sx={{ display: "flex", justifyContent: "end", mt: 4 }}>
+        <FormControl fullWidth>
+          <InputLabel>Room</InputLabel>
+          <Select defaultValue="ROOM001" value={room} label="Approval" onChange={handleRoom}>
+            <MenuItem value="ROOM001">ROOM001</MenuItem>
+            <MenuItem value="ROOM002">ROOM002</MenuItem>
+            <MenuItem value="ROOM003">ROOM003</MenuItem>
+            <MenuItem value="ROOM004">ROOM004</MenuItem>
+            <MenuItem value="ROOM005">ROOM005</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      {isLoading && events ? <p>Loading...</p> : <BigCalendar events={events} />}
+      {error && <p>Error fetching data</p>}
+      {/* <CardEvent /> */}
     </>
   );
 };
